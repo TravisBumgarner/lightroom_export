8d2fee3c268c7484fe1078b1c91458d77470ebdb
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-05-24 22:42:51 -0600

    updated for win

---
+++ main_win.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+source_folder = r"Z:\Dropbox"
+
+# Destination folder for exported photos
+destination_folder = r"Z:\mobile_lib"
+
+def move_exported_photos(src, dst):
+    for root, dirs, files in os.walk(src):
+        for file in files:
+            if "_exported_for_viewing_locally" in file:
+                source_path = os.path.join(root, file)
+                # Get the relative path within the source folder
+                relative_path = os.path.relpath(source_path, src)
+                destination_path = os.path.join(dst, relative_path)
+                destination_dir = os.path.dirname(destination_path)
+                
+                # Create the destination directory if it doesn't exist
+                os.makedirs(destination_dir, exist_ok=True)
+                
+                shutil.move(source_path, destination_path)
+                print(f"Moved: {source_path} to {destination_path}")
+
+# Create the destination folder if it doesn't exist
+if not os.path.exists(destination_folder):
+    os.makedirs(destination_folder)
+
+# Call the function to move exported photos
+move_exported_photos(source_folder, destination_folder)
+w
